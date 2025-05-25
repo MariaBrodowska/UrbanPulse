f340be206a3feedfbe36bb2559ffe8820e35802d
Maria <mariabrodowska89@gmail.com>
2025-05-25 10:14:39 +0200

    configure CORS to allow frontend requests

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -16,6 +16,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Konfiguracja AppContext dla UTC
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", false);
 
+//corsy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy
+            .WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials());
+});
 
 builder.Services.AddControllers();
 
@@ -64,6 +74,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseCors("AllowFrontend");
 app.MapControllers();
 
 app.MapGet("/", ([FromServices] GeneringDataService dataService) =>

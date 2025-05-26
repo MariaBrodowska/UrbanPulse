fecda81ddb7d23d97dcbe69d051728bbfe3066de
ThePowerOf76 <s99507@pollub.edu.pl>
2025-05-26 22:15:25 +0200

    Fixed unresolved merge conflict in the main app file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,14 @@ import './App.css'
 import { Route, Routes } from "react-router-dom"
 import DisplayRegisterPage from './user/register/Register'
 import DisplayLoginPage from './user/login/Login'
-<<<<<<< Updated upstream
-import TokenProvider from './user/login/Token'
-import CheckToken from './user/CheckToken'
-=======
 import DisplayDatasetsPage from './data/datasets/Datasets'
->>>>>>> Stashed changes
 function App() {
   return (
-    <TokenProvider>
       <Routes>
-        <Route path="/checktoken" element={<CheckToken />} />
         <Route path="/login" element={<DisplayLoginPage />} />
         <Route path="/register" element={<DisplayRegisterPage />} />
         <Route path="/datasets" element={<DisplayDatasetsPage />}  />
       </Routes>
-    </TokenProvider>
   )
 }
 export default App

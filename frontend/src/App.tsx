30ad459d817ac7732ce69bc448e0940b158efe33
ThePowerOf76 <s99507@pollub.edu.pl>
2025-05-28 19:32:21 +0200

    Add chart logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom"
 import DisplayRegisterPage from './user/register/Register'
 import DisplayLoginPage from './user/login/Login'
 import DisplayDatasetsPage from './data/datasets/Datasets'
+import DataCharts from './data/charts/Charts'
 function App() {
   return (
-      <Routes>
-        <Route path="/login" element={<DisplayLoginPage />} />
-        <Route path="/register" element={<DisplayRegisterPage />} />
-        <Route path="/datasets" element={<DisplayDatasetsPage />}  />
-      </Routes>
+    <Routes>
+      <Route path="/login" element={<DisplayLoginPage />} />
+      <Route path="/register" element={<DisplayRegisterPage />} />
+      <Route path="/datasets" element={<DisplayDatasetsPage />} />
+      <Route path="/charts" element={<DataCharts />} />
+    </Routes>
   )
 }
 export default App
